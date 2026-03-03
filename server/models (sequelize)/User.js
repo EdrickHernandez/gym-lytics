97d759de927ebfc269f1481645d624e38cea4340
edrick <edrickshl@gmail.com>
2026-03-03 20:15:18 -0300

    creando archivo User.js

---
+++ server/models (sequelize)/User.js
@@ -0,0 +1,65 @@
+const { DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('./index');
+
+const User = sequelize.define('User', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  email: {
+    type: DataTypes.STRING(255),
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true,
+    },
+  },
+  password: {
+    type: DataTypes.STRING(255),
+    allowNull: false,
+  },
+  nombre: {
+    type: DataTypes.STRING(100),
+    allowNull: false,
+  },
+  edad: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+  },
+  peso: {
+    type: DataTypes.DECIMAL(5, 2),
+    allowNull: false,
+  },
+  altura: {
+    type: DataTypes.DECIMAL(5, 2),
+    allowNull: false,
+  },
+}, {
+  tableName: 'users',
+  timestamps: true,
+  createdAt: 'created_at',
+  updatedAt: 'updated_at',
+  hooks: {
+    beforeCreate: async (user) => {
+      if (user.password) {
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(user.password, salt);
+      }
+    },
+    beforeUpdate: async (user) => {
+      if (user.changed('password')) {
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(user.password, salt);
+      }
+    },
+  },
+});
+
+// Método para comparar contraseñas
+User.prototype.comparePassword = async function(candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
+
+module.exports = User;
